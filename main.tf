ae6a82ccb6668ef0093047ec7fad272ef6fa8c33
sadanand-cloud-tech <msadha431@gmail.com>
2025-11-19 15:33:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "dev" {
   ami    = "ami-0fa3fe0fa7920f68e"
     instance_type = "c7i-flex.large"
+   subnet_id = "subnet-01b4eec2c80252c6b" 
     tags = {
       Name = "prod"
     }
